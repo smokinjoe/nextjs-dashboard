eaaf4980730f95868963e4afa9a9b8d02a7715c2
Joe <joe.ekiert@gmail.com>
2024-12-15 15:54:27 -0500

    chapter 12 mutating data

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,34 @@
+import Form from "@/app/ui/invoices/edit-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { customers } from "@/app/lib/placeholder-data";
+
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function Page(props: PageProps) {
+  const params = await props.params;
+  const { id } = params;
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
